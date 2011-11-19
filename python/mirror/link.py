96b757686aa02c792195b9438f0419bdefce7030
Conor Sayres <csayres@uw.edu>
2011-11-18 16:59:36 -0800

    new file structure

---
+++ python/mirror/link.py
@@ -0,0 +1,232 @@
+"""Links
+
+Mirrors are constrained by a set of 6 links: a mix of actuators and fixed-length links.
+
+Links have two pivot points that are assumed to be perfect ball joints:
+one attached to the mirror, the other attached to the mirror support structure.
+These are defined by cartesian coordinates mirPos and basePos.
+
+Links are either adjustable (actuators or encoders) or have fixed length.
+Important attributes of adjustable links:
+- physical length: length of actuator/sensor in mm
+    0 when the mirror is at zero orientation
+    + makes the actuator longer
+- mount length: length of actuator/sensor in native units: microsteps or ticks;
+    this differs from mount by an offset and a scale:
+    mount = offset + (scale * phys)
+- length: the distance from the base ball joint to the mirror ball joint
+
+There are three kinds of links supported in this file:
+- FixedLengthLink: length does not change
+- AdjLengthLink: length between base and mount changes. These are easy to model.
+  They are the traditional hexapod actuators. The old TCC assumes all actuators
+  are this kind, but we don't actually have any.
+- AdjBaseLink: base pistons. This actuator introduces the concept of the home base position
+  and the current base position. The secondary and tertiary mirrors use these.
+
+Notes:
+- The difference between AdjLengthLink and AdjBaseLink may be small enough to be ignored.
+  AdjBaseLink is simpler.
+- The 2.5m primary mirror has pistons that touch glass without being attached to it.
+  Either AdjLengthLink or AdjBaseLink is probably a usable model for this.
+- Links have a mirror position that is a ball joint attached somewhere to the mirror,
+  or in some cases to the back of a shaft along which the mirror can piston.
+- Links have a base position that is a ball joint fixed to a frame.
+
+Actuators and Encoders:
+- Actuators are links that control the length of the adjustable element
+- Encoders are links that sense the length of the adjustable element
+- Mirrors must have exactly 6 actuators or fixed-length links
+- Mirrors also have 0 or 1 encoder for each actuator. The encoder does not have to be
+  perfectly coaligned, but is assumed to be somewhat nearby.
+"""
+import numpy
+import math
+
+class BaseLink(object):
+    """
+    A base class for linear actuators and fixed links.
+    Links are controlled in mount coordinates = steps, but all positions are in mm.
+    """
+    def __init__(self, isAdjustable, basePos, mirPos):
+        """
+        Inputs:
+        - isAdjustable: True for actuators, False for fixed links.
+        - basePos:      cartesian position of end of actuator fixed to the base (mm)
+        - mirPos:       cartesian position of end of actuator attached to the mirror (mm)
+                        when the mirror is at zero orientation
+        
+        Zero orientation means piston, x tilt, y tilt, x translation, y translation and z rotation
+        are all zero.
+        """
+        self.isAdjustable = bool(isAdjustable)
+        self.basePos = numpy.asarray(basePos, dtype=float)
+        if self.basePos.shape != (3,):
+            raise RuntimeError("basePos=%s must be 3 elements" % (basePos,))
+        self.mirPos = numpy.asarray(mirPos, dtype=float)
+        if self.mirPos.shape != (3,):
+            raise RuntimeError("mirPos=%s must be 3 elements" % (mirPos,))
+        self.neutralLength = self.lengthFromMirPos(self.mirPos)
+    
+#     def physFromMirPos(self, mirPos):
+#         """Compute physical length (mm) of adjustable element given the mirror position (mm)
+#         
+#         Subclasses must override.
+#         """
+#         raise NotImplementedError("Subclasses must define")
+
+    def lengthFromMirPos(self, mirPos):
+        """
+        Return the desired length of link in mm.
+        
+        This is the distance between the supplied mirPos and the original basePos.
+        The link may not be capable of achieving this length.
+        """        
+        return numpy.linalg.norm(self.basePos - mirPos)
+
+
+class FixedLengthLink(BaseLink):
+    """A fixed-length link with two ball joints: one attached to a base, the other attached to a mirror
+    """
+    def __init__(self, basePos, mirPos):
+        BaseLink.__init__(self, isAdjustable=False, basePos=basePos, mirPos=mirPos)
+    
+#     def physFromMirPos(self, mirPos):
+#         """Compute physical length (mm) of adjustable element given the mirror position (mm)
+#         """
+#         return 0.0
+
+    def physFromMirPos(self, mirPos):
+        """Compute physical length (mm) of adjustable element given the mirror position (mm)
+        """
+        length = self.lengthFromMirPos(mirPos)
+        return length - self.neutralLength
+
+
+class AdjustableLink(BaseLink):
+    """Generic adjustable-length actuator: a base class for specific designs.
+    """
+    def __init__(self, basePos, mirPos, minMount, maxMount, scale, offset):
+        """
+        Inputs:
+        - basePos:  cartesian position of end of actuator fixed to the base (mm)
+        - mirPos:   cartesian position of end of actuator attached to the mirror (mm)
+                    when the mirror is at zero orientation
+        - minMount: minimum length (steps)
+        - maxMount: maximum length (steps)
+        - scale:    actuator scale (mm/step)
+        - offset:   mount position when mirror is at zero orientation
+        """
+        BaseLink.__init__(self, isAdjustable=True, basePos=basePos, mirPos=mirPos)
+        self.minMount = float(minMount)
+        self.maxMount = float(maxMount)
+        self.scale = float(scale)
+        self.offset = float(offset)
+        # Unit vector defining the axis of mount motor
+        self.pistonDir = (self.mirPos - self.basePos) / numpy.linalg.norm((self.mirPos - self.basePos))
+
+    
+    def mountFromPhys(self, phys):
+        """Compute mount length (steps) of adjustable element given its physical length (mm)
+        """
+        return self.offset + (self.scale * phys)
+    
+    def physFromMount(self, mount):
+        """Compute physical length (mm) of adjustable element given its mount length (steps)
+        """
+        return (mount - self.offset) / self.scale
+    
+    def mountInRange(self, mount):
+        """Return True if the mount position is in range
+        """
+        return self.minMount <= mount <= self.maxMount
+
+
+class AdjLengthLink(AdjustableLink):
+    """Adjustable-length actuator in which the distance between the two ball joints varies.
+    """
+    def physFromMirPos(self, mirPos):
+        """Compute physical length (mm) of adjustable element given the mirror position (mm)
+        """
+        length = self.lengthFromMirPos(mirPos)
+        return length - self.neutralLength
+
+
+class AdjBaseActuator(AdjLengthLink):
+    """Adjustable-base actuator. The base joint pistons.
+    
+    The direction of piston of the base is assumed to be along a vector
+    pointing from basePos to mirPos at mirror neutral position.
+
+    Right now there are three different methods to determine conversions, we should test and pick one
+    (or none and just use AdjLengthActuator).
+    """
+#     def physFromMirPos(self, mirPos):
+#         """Compute physical length (mm) of adjustable element given the mirror position (mm)
+#         
+#         This computation is exact but requires trig and so will be slow
+#         """
+#         # this method is producing math domain errors, I think it has to do 
+#         # with machine precision
+#         r_naught = self.neutralLength
+#         
+#         # Calculate vector from basePos to a given mirPos
+#         r = (mirPos - self.basePos)
+#         # Get projection of mirVec (link axis) along axis of motor mount.
+#         x = numpy.dot(r, self.pistonDir)
+#         
+#         y = numpy.linalg.norm(numpy.cross(r, self.pistonDir))
+#         try: 
+#             return x - (r_naught * math.cos(math.asin(y / r_naught)))
+#         except ValueError:
+#             raise RuntimeError( 'Err: lim of y = r_naught, but y=%5.3f r_n=%5.3f' % (y, r_naught))
+
+    def physFromMirPosCCS(self, mirPos):
+        """Compute physical length (mm) of adjustable element given the mirror position (mm)
+
+        This method makes the approximation that the projection of the 'true
+        length' from the actuator motor axis to the link axis is equal to the
+        difference between length at zero orientation and the length at a given
+        mirror position. This should be ok for small angles, and the solution
+        requires no trig functions.
+        
+        Inputs:
+        - mirPos: cartesian position of end of actuator attached to the mirror (mm).
+        
+        Output:
+        - phys: approximate actuator length (mm)
+        """
+        # Calculate unit vector from basePos to a given mirPos (link axis).
+        mirVec = mirPos - self.basePos
+        mirUnitVec = mirVec / numpy.linalg.norm(mirVec)
+        phys = self.physFromMirPos(mirVec)
+        # Use right triangle trig to solve for truePhys value.
+        # cos(theta) = adjacent / hypotenuse
+        # theta = acos(self.pistonDir dot mirUnitVec)
+        # adjacent = phys
+        # hypotenuse = truePhys
+        truePhys = phys / numpy.dot(self.pistonDir, mirUnitVec)
+        return truePhys
+               
+    def physFromMirPos(self, mirPos):
+        """Compute physical length (mm) of adjustable element given the mirror position (mm)
+        
+        Inputs:
+        - mirPos: cartesian position of end of actuator attached to the mirror (mm).
+        
+        Output:
+        - phys: approximate actuator length (mm)
+        """
+        r_not = self.neutralLength
+        
+        # Calculate vector from basePos to a given mirPos
+        r = mirPos - self.basePos
+
+        # Get projection of mirVec (link axis) along axis of motor mount.
+        x = numpy.dot(r, self.pistonDir)
+        
+        # Get projection of mirVec (link axis) along axis perpendicular to motor mount.
+        yVec = numpy.cross(r, self.pistonDir)
+        ySq = numpy.sum(yVec * yVec)
+        
+        return x + (ySq / (2.0 * r_not)) - r_not
---
+++ test/mir_unittest.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+import unittest
+import numpy
+import math
+import time
+
+from data import genMirData
+import mirror
+
+MMPerMicron = 1 / 1000.0        # millimeters per micron
+RadPerDeg  = math.pi / 180.0    # radians per degree
+ArcSecPerDeg = 60.0 * 60.0      # arcseconds per degree
+RadPerArcSec = RadPerDeg / ArcSecPerDeg # radians per arcsec
+
+#Define maximum allowable orient error, reusing from mirror.py, not using z rot
+# maxOrientErr = numpy.array([0.0001, 5e-8, 5e-8, 0.0001, 0.0001])
+maxOrientErr = numpy.array([0.1, 5e-3, 5e-3, 0.1, 0.1])
+maxMountErr = 0.1
+
+# construct range of orients to test (0-25mm, 0-2 degrees)
+maxDist = 25 #mm
+maxTilt = 2 # degrees
+resolution = 10
+dist_range = numpy.linspace(0, maxDist, resolution)
+ang_range = numpy.linspace(0, maxTilt * RadPerDeg, resolution)
+ranges = [dist_range, ang_range, ang_range, dist_range, dist_range]
+orientRange = numpy.zeros((resolution * 5, 5))
+for ind, rng in enumerate(ranges):
+    orientRange[ind*resolution:ind*resolution+resolution, ind] = rng
+    
+# construct a set of random orientations.
+num=5
+orientRand = numpy.zeros((num, 5))
+distRand = numpy.random.random_sample(num) * maxDist # pist, transXY
+tiltRand = numpy.random.random_sample(num) * maxTilt * RadPerDeg
+orientRand[:,[0, 3, 4]] = numpy.vstack((distRand, distRand, distRand)).T
+orientRand[:,[1, 2]] = numpy.vstack((tiltRand, tiltRand)).T
+
+# choose what type of orientation set you want to use in tests, right now there are:
+# orientRange and orientRand
+orientList = orientRange  
+
+# construct a list of all mirrors, with all actuator options.
+# from genMirData:
+# prim25List[8]: includes 2 faked fixed link versions and faked encoders
+# sec25List[4]: includes a faked fixed link and faked encoders
+# sec35List[4]: includes a faked fixed link and faked encoders
+# tert35List[2]: 
+
+
+prim25 = [mirror.DirectMirror(*input) for input in genMirData.prim25List]
+secCtrMirZ = -135.70
+secCtrBaseZ = -178.40
+sec25 = [mirror.TipTransMirror(secCtrMirZ, secCtrBaseZ, *input) for input in genMirData.sec25List]
+sec35 = [mirror.DirectMirror(*input) for input in genMirData.sec35List]
+tert35 = [mirror.DirectMirror(*input) for input in genMirData.tert35List]
+# choose which mirrors you want to include in the tests
+mirList = tert35
+
+############################# TESTS #####################################
+
+class MirTests(unittest.TestCase):
+    """Tests for mirrors
+    """
+
+    def testRoundTripAct(self):
+        linkType = 'Act'
+        errLog = self._roundTrip(linkType)
+        self._errLogPrint(errLog, linkType)
+        self.assertEqual(len(errLog), 0, 'Errors Found and Printed To File')
+
+        
+    def testRoundTripEnc(self):
+        linkType = 'Enc'
+        errLog = self._roundTrip(linkType)
+        self._errLogPrint(errLog, linkType)
+        self.assertEqual(len(errLog), 0, 'Errors Found and Printed To File')
+        
+    def _roundTrip(self, linkType):
+                
+        errLog=[]
+        for mirIter, mir in enumerate(mirList):
+            if linkType == 'Enc':
+                mountFromOrient = mir.encoderMountFromOrient
+                orientFromMount = mir.orientFromEncoderMount
+            elif linkType == 'Act':
+                mountFromOrient = mir.actuatorMountFromOrient
+                orientFromMount = mir.orientFromActuatorMount
+                
+            for orientIn in orientList:
+                try:
+                    mnt = mountFromOrient(orientIn)
+                    orient = orientFromMount(mnt)
+                    mnt2 = mountFromOrient(orient)
+                except RuntimeError as er:
+                    errLog.append(self._fmtRunTimeErr(er, orientIn, mirIter))
+                    continue
+                orientErr, failStr = self._checkOrient(orientIn, orient, mirIter)
+                if True in (orientErr > maxOrientErr):
+                    errLog.append(failStr)
+                    continue 
+                mountErr, failStr = self._checkMount(orientIn, mnt, mnt2, mirIter)
+                if True in (mountErr > maxMountErr):
+                    errLog.append(failStr)
+        return errLog            
+
+    def _fmtRunTimeErr(self, error, orientIn, mirIter):
+        """For catching and printing a RuntimeError
+        """
+        str ='Mirror number: %2.0f orientIn: %10.4f %10.4f %10.4f %10.4f %10.4f' %\
+                               (mirIter, orientIn[0], orientIn[1]/RadPerArcSec, orientIn[2]/RadPerArcSec,
+                                orientIn[3], orientIn[4], mirIter) + str(er) + '\n'
+        return str
+        
+    def _errLogPrint(self, errLog, linkType):
+        """Print the error log to a file
+        """
+        fileDate = time.localtime()
+        fileName = 'Errors' + linkType + '_' + str(fileDate[0]) + str(fileDate[1])\
+                     + str(fileDate[2]) + str(fileDate[3]) +\
+                    str(fileDate[4]) + str(fileDate[5]) +'.txt'
+        with open(fileName, 'w') as f:
+            for line in errLog:
+                f.write(line)
+                            
+    def _checkOrient(self, orientIn, orientOut, mirIter):
+        """This checks if the value abs(orientIn - orientOut) is within
+        the limit defined by maxOrientErr.  If it isn't the test fails,
+        and the orientIn and Errors are printed """
+        orientIn = numpy.asarray(orientIn[0:5], dtype=float) # only 5 axes (no rotZ)
+        orientOut = numpy.asarray(orientOut[0:5], dtype=float) # only 5 axes (no rotZ)
+        orientErr = numpy.abs(orientIn - orientOut)
+        failStr = 'Mirror number: %s orientIn(5):  %9.4f %9.4f %9.4f %9.4f %9.4f orientErr(5): %9.4f %9.4f %9.4f %9.4f %9.4f\n' %\
+               (mirIter, orientIn[0], orientIn[1]/RadPerArcSec, orientIn[2]/RadPerArcSec,
+               orientIn[3], orientIn[4], orientErr[0],
+               orientErr[1]/RadPerArcSec, orientErr[2]/RadPerArcSec,
+               orientErr[3], orientErr[4])
+        return orientErr, failStr
+        #self.assertFalse(True in (orientErr > maxOrientErr), failStr)
+
+    
+    def _checkMount(self, orientIn, mountIn, mountOut, mirIter):
+        """This compares the differnce between the mount computed from
+        initial orient and mount computed from recovered orient.  This
+        will check that the fitting process produced the desired
+        orientation, and not an alternate one """
+        orientIn = numpy.asarray(orientIn[0:5], dtype=float) # only 5 axes (no rotZ)
+        mountIn = numpy.asarray(mountIn, dtype=float)
+        mountOut = numpy.asarray(mountOut, dtype=float)
+        mountErr = numpy.abs(mountIn - mountOut)
+        # append zero error to make len(mountErr) == 6
+        if len(mountErr) < 6:
+            mountErr = numpy.hstack((mountErr, numpy.zeros(6-len(mountErr))))
+        failStr = 'Mirror number: %s orientIn(5):  %9.4f %9.4f %9.4f %9.4f %9.4f mountErr(6): %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f\n' %\
+               (mirIter, orientIn[0], orientIn[1]/RadPerArcSec, orientIn[2]/RadPerArcSec,
+               orientIn[3], orientIn[4], mountErr[0],
+               mountErr[1], mountErr[2],
+               mountErr[3], mountErr[4], mountErr[5])
+        return mountErr, failStr
+        #self.assertFalse(True in (mountErr > maxMountErr), failStr)
+   
+
+                
+                
+if __name__ == '__main__':
+    unittest.main()
