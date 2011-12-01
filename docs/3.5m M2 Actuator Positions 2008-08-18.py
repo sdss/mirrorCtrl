6e9ce6a89ef2882bf1775314b09089c5c07aed40
Conor Sayres <csayres@uw.edu>
2011-12-01 08:51:41 -0800

    added docs

---
+++ docs/3.5m M2 Actuator Positions 2008-08-18.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+import numpy
+import math
+"""3.5m M3 mirror actuator worksheet
+
+Compute data for the Galil file "Constants 35m M2.gal" and the TCC file tdat:mir.dat
+
+The TCC code assumes that a flexure is attached at one end to the base
+and the other end to the mirror, and that the actuator makes the flexure longer or shorter
+Thus this worksheet computes the position of the flex point at each end of the flexure
+when the mirror is in its neutral position (centered).
+However, the 3.5m actuators actually work by having a linear actuator
+(which is attached to the base) move the "base" end of the flexure along a line.
+This the TCC's model is only an approximation.
+
+Basics:
+- all distances are in mm and all angles are in degrees unless otherwise noted
+- all numbers are for the 3.5m secondary mirror support system
+- X = right, Y = up, Z = from the sky towards the telescope
+where up/down/left/right are as seen with the telescope at the horizon,
+  standing behind the primary mirror, looking towards the secondary mirror.
+"""
+MMPerInch = 25.4
+RadPerDeg = math.pi / 180.0
+
+def printTCCLine(name, axVal, trVal, fmtStr):
+    axStr = fmtStr % axVal
+    trStr = fmtStr % trVal
+    print name, " ".join([axStr]*3), " ".join([trStr]*2)
+    
+def printTCCPosTable(name, pos):
+    """Print a table of actuator positions.
+
+    Inputs:
+    - name: one of "Mir" or "Base"
+    - pos: a matrix [actInd, coordInd]
+    """
+    for coordInd, coordName in enumerate(("X", "Y", "Z")):
+        infoTuple = (name, coordName,) + tuple(pos[:,coordInd])
+        print "SecAct%s%s %10.3f %10.3f %10.3f %10.3f %10.3f" % infoTuple
+
+def getMirGeometry():
+    """Return mirror geometry for mirror attachment and base attachment points
+    
+    Returns:
+    - mirPos[actInd, coordInd]
+    - basePos[actInd, coordInd]
+    where actInd is:
+    - 0: Axial A
+    - 1: Axial B
+    - 2: Axial C
+    - 3: Transverse D
+    - 4: Transverse E
+    (the rotational constraint is implied)
+    """
+    
+    """Axial actuator geometry
+    
+    There are three axial actuators perpendicular to the mirror
+    and spaced evenly at 120 degree angles
+    """
+    axRad =    10.480 * MMPerInch   # radius from axMirror center to axial encoders
+    axMirZ =   -6.016 * MMPerInch   # z position of axial actuator mirror attachment
+    axBaseZ = -10.096 * MMPerInch   # z position of axial actuator base attachment
+    axAngA = 90.0   # angle from X to Y axis of axial actuator A
+    angDegList = numpy.arange(axAngA, 359.0, 360.0 / 3.0)
+    angRadList = angDegList * RadPerDeg
+    
+    mirPos = numpy.zeros([5,3])
+    basePos = mirPos.copy()
+    for actInd, angRad in enumerate(angRadList):
+        mirPos[actInd, :] = numpy.array((
+            math.cos(angRad) * axRad,
+            math.sin(angRad) * axRad,
+            axMirZ
+        ))
+        basePos[actInd, 0:2] = mirPos[actInd, 0:2]
+        basePos[actInd, 2] = axBaseZ
+    
+    """Transverse Actuator geometry
+    
+    Transverse motion is controlled by two actuators that attach
+    at the mirror end near the center of the back of the mirror
+    and at the edge of the cage are pulled by Eastern Air Devices
+    linear actuators (motors that turn hollow nuts)
+    The encoders sense the extension of the screw that goes into the linear actuator
+    I approximate this by assuming the flexure gets longer or shorter
+    and that the encoder directly measures the length of the flexure
+    1: Transverse D is upper right
+    2: Transverse E is upper left
+    """
+    trMirZ   = -6.589 * 25.4    # z position of mirror end of transverse actuator flexure
+    trBaseZ  = -(7.337 + 7.837) * 25.4 / 2  # z position of base end of transverse actuator
+    trMirRad =  1.625 * 25.4    # radial distance from center of mirror to mirror end of transverse actuator flexure
+    trBaseRad = 15.813 * 25.4   # radial distance from center of mirror to base end of transverse actuator
+    trAng = numpy.array((45.0, 135.0))
+    
+    for actInd, trAngDeg in enumerate((45.0, 135.0)):
+        trInd = actInd + 3
+        trAngRad = trAngDeg * RadPerDeg
+        mirPos[trInd, :] = (
+            trMirRad * math.cos(trAngRad),
+            trMirRad * math.sin(trAngRad),
+            trMirZ
+        )
+        basePos[trInd, :] = (
+            trBaseRad * math.cos(trAngRad),
+            trBaseRad * math.sin(trAngRad),
+            trBaseZ
+        )
+    return mirPos, basePos
+
+"""
+Actuator resolution and range of motion
+"""
+
+# Actuator scale (resolution)
+# Axial: 40 thread/inch * 80:1 harmonic drive reduction * 200 full steps/rev * 50 microstep/step
+axScaleMicrostepPerInch = 40 * 80 * 200 * 50 
+axScaleMicrostepPerMicrometer = axScaleMicrostepPerInch / (25.4 * 1000)
+
+# Transverse: -- 80 thread/inch * 200 full steps/rev * 50 microstep/full step
+trScaleMicrostepPerInch = 80 * 200 * 50 
+trScaleMicrostepPerMicrometer = trScaleMicrostepPerInch / (25.4 * 1000)
+
+# Encoder resolution
+# Axial: 2 um period, 200x interpolation
+axEncUMPerTick = 2 * 200
+axEncMicrostepPerTick = axScaleMicrostepPerMicrometer / axEncUMPerTick
+
+# Transverse: 2 um period, 10x interpolation
+trEncUMPerTick = 2 * 10
+trEncMicrostepPerTick = trScaleMicrostepPerMicrometer / trEncUMPerTick
+
+actScale = (axScaleMicrostepPerMicrometer,)*3 + (trScaleMicrostepPerMicrometer,) * 2
+
+# Actuator range of motion
+axFullRangeMicrostep = 15300000
+trFullRangeMicrostep = 200000
+axMarginInch = 0.0125
+trMarginInch = 0.00625
+axMarginMicrostep = int(0.5 + (axMarginInch * axScaleMicrostepPerInch))
+trMarginMicrostep = int(0.5 + (trMarginInch * trScaleMicrostepPerInch))
+axUsableRangeMicrostep = axFullRangeMicrostep - (2 * axMarginMicrostep)
+trUsableRangeMicrostep = trFullRangeMicrostep - (2 * trMarginMicrostep)
+
+print "\nData for \"Constants 35m M2.gal\""
+
+print """
+NO: distance between software limit and limit switch (microsteps)
+MARGA = %d;  NO: %.4f inches
+MARGB = MARGA
+MARGC = MARGA
+MARGD = %d;  NO: %.5f inches
+MARGE = MARGD""" % (axMarginMicrostep, axMarginInch, trMarginMicrostep, trMarginInch)
+
+print """
+NO: full range of motion (microsteps)
+RNGA = %d - (2 * MARGA)
+RNGB = RNGA
+RNGC = RNGA
+RNGD = %d - (2 * MARGD)
+RNGE = RNGD""" % (axFullRangeMicrostep, trFullRangeMicrostep)
+
+print """
+NO: resolution of encoder (microsteps/encoder tick), 0 if no encoder
+NO: for a servo motor, this is the auxiliary encoder
+ENCRESA = %.4f
+ENCRESB = ENCRESA
+ENCRESC = ENCRESA
+ENCRESD = %.4f
+ENCRESE = ENCRESD""" % (axEncMicrostepPerTick, trEncMicrostepPerTick)
+
+mirPos, basePos = getMirGeometry()
+
+print "\nData for TCC file tdat:mir.dat\n"
+print "! Limits of motion (mount units) for each actuator"
+printTCCLine("SecMinMount", -axUsableRangeMicrostep/2, -trUsableRangeMicrostep/2, "%10d")
+printTCCLine("SecMaxMount",  axUsableRangeMicrostep/2,  trUsableRangeMicrostep/2, "%10d")
+print "! Offset (mount units) and scale (mount units/um) for each actuator"
+print "SecMountOffset     0          0          0          0          0"
+printTCCLine("SecMountScale", axScaleMicrostepPerMicrometer, trScaleMicrostepPerMicrometer, "%10.3f")
+print """! All distances are in mm, all angles are in degrees
+! All numbers are for the 3.5m secondary mirror support system
+! X = right, Y = up, Z = from the sky towards the telescope
+! where up/down/left/right are as seen with the telescope at the horizon,
+! standing behind the primary mirror, looking towards the secondary mirror.
+!              Ax A       Ax B       Ax C       Tr D       Tr E"""
+printTCCPosTable("Mir", mirPos)
+printTCCPosTable("Base", basePos)
---
+++ docs/3.5m M3 Actuator Positions 2008-04-18.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import numpy
+import math
+"""3.5m M3 mirror actuator geometry
+
+All references are as seen looking at the mirror from the instrument port,
+with "up" being towards the secondary mirror.
+
+Positions from Larry Carey were provided in the following "in-plane" coordinate system,
+with X and Y in the plane of the mirror:
++X is to the right
++Z is from back of mirror to front of mirror
++Y is as required for right-handed: up and back
+
+The actuators and encoders are lettered in a counter-clockwise direction, with A down.
+(This is a 180 degree rotation from the positioning of the old tertiary actuators.)
+
+The Heidenhains are on a radius of 11.714 in. from the origin, 120 deg. apart,
+azmuthally aligned with the axial actuators. (The actuators are on a radius of 8.963")
+
+Using the above convention, the nominal positions of the "flex points" for the Haidenhains are as follows, in 
+inches (warning: the TCC uses mm):
+
+Encoder    X         Y         Z Mir      Z Base
+A          0       -11.714    -0.875     -3.375
+B         10.145     5.857    -0.875     -3.375
+C        -10.145     5.857    -0.875     -3.375
+
+(Note: the distance from the back of the back face to the vertex of the mirror is approximately 4.97",
+but the Heidenhain encoders are attached to the back of the front plate.)
+
+The coordinate system used by the TCC as follows:
++Z points towards the secondary mirror
++Y points towards the instrument port
++X is as required for a right-handed coordinate system
+   (this is to the right as seen by the instrument if Z is up)
+which is the in-plane coordinate system rotated -45 degrees about the X axis.
+
+So the positions are first computed "in plane", then rotated to give the values used by the TCC.
+"""
+
+MMPerInch = 25.4
+RadPerDeg = math.pi / 180.0
+
+def printTable(name, pos):
+    """Print a table of actuator positions.
+
+    Inputs:
+    - name: one of "Mir" or "Base"
+    - pos: a matrix [actInd, coordInd]
+    """
+    for coordInd, coordName in enumerate(("X", "Y", "Z")):
+        infoTuple = (name, coordName,) + tuple(pos[:,coordInd])
+        print "TertAct%s%s %10.3f %10.3f %10.3f %10.3g %10.3g %10.3g" % infoTuple
+    
+rad =   11.714 * MMPerInch
+zMir =  -0.875 * MMPerInch
+zBase = -3.375 * MMPerInch
+angDegList = numpy.arange(-90.0, 359.0, 360.0 / 3.0)
+angRadList = angDegList * RadPerDeg
+
+# compute in-plane positions
+# with convention:
+# z points from back of glass to front of glass
+# y points sort of towards the instrument
+# x is as required for right-handed coordinate system
+# Actuator index:
+#   0-2 = axial actuators A-C
+#   3-5 = constraints
+mirIP = numpy.zeros([6,3])
+baseIP = mirIP.copy()
+# first handle actuators A, B and C
+for actInd, angRad in enumerate(angRadList):
+    mirIP[actInd, :] = numpy.array((
+        math.cos(angRad) * rad,
+        math.sin(angRad) * rad,
+        zMir
+    ))
+    baseIP[actInd, 0:2] = mirIP[actInd, 0:2]
+    baseIP[actInd, 2] = zBase
+    
+# now add constraints:
+# 3,4 are transverse
+# 5 is anti-rotation
+mirIP[3] = numpy.zeros(3)
+mirIP[4] = mirIP[3].copy()
+mirIP[5] = mirIP[3].copy()
+mirIP[5,0] = rad
+baseIP[3] = mirIP[3].copy()
+baseIP[3, 0:2] = (1.0e9, 1.0e9)
+baseIP[4, 0:2] = (-1.0e9, 1.0e9)
+baseIP[5] = (rad, 1.0e9, 0)
+
+print "Sanity check; unrotated positions in inches"
+printTable("UMirInch", mirIP / MMPerInch)
+print
+printTable("UBaseInch", baseIP / MMPerInch)
+print
+
+# rotate to final coordinate system which is:
+# z points towards secondary
+# y points towards the instrument port
+# x is unchanged
+# in other words, rotate 45 degrees about x
+rotAng = -45.0 * math.pi / 180.0
+rotMat = numpy.zeros([3,3])
+rotMat[0,0] = 1
+rotMat[1,1] = math.cos(rotAng)
+rotMat[1,2] = math.sin(rotAng)
+rotMat[2,2] = rotMat[1,1]
+rotMat[2,1] = -rotMat[1,2]
+
+mirPos = numpy.dot(mirIP, rotMat)
+basePos = numpy.dot(baseIP, rotMat)
+
+print "Data for tdat:mir.dat"
+printTable("Mir", mirPos)
+printTable("Base", basePos)
+
+
