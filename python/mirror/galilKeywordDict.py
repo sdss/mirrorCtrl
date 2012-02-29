2b40d990979e0a1953a31f279d802ccbaf6a818c
Conor Sayres <csayres@uw.edu>
2012-02-29 07:54:30 -0800

    in progress

--- python/mirror/galilKeywordDict.py
+++ python/mirror/galilKeywordDict.py
@@ -1,32 +1,59 @@
-KeysDictionary("galil", (?,?), 
+KeysDictionary("galilactor", (1,1), 
 # version...
 
 # parameters
     Key("DesOrient",
-        float(help = "Piston", units = "um"),
-        float(help = "Tilt X", units = "arcseconds"),
-        float(help = "Tilt Y", units = "arcseconds"),
-        float(help = "Translation X", units = "um"),
-        float(help = "Translation Y", units = "um"),
-        float(help = "Rotation about Z", units = "arcseconds"),
-        help = "The desired mirror orientation"
+        Float(invalid="nan", help = "Piston", units = "um"),
+        Float(invalid="nan", help = "Tilt X", units = "arcseconds"),
+        Float(invalid="nan", help = "Tilt Y", units = "arcseconds"),
+        Float(invalid="nan", help = "Translation X", units = "um"),
+        Float(invalid="nan", help = "Translation Y", units = "um"),
+        Float(invalid="nan", help = "Rotation about Z", units = "arcseconds"),
+        help = "The desired mirror orientation."
     ),
     Key("DesOrientAge", 
-        float(invalid = "nan", units = "seconds"),
-        help = "Elapsed time since a desired orientation was set"
+        Float(invalid = "nan", units = "seconds"),
+        help = "Elapsed time since a desired orientation was set."
+    ),    
+    Key("CmdMount",
+        Float(invalid = "nan")*(nAct,),
+        help = "Commanded mount positions."
     ),
-    
-CmdMount = float[nAct]
-Cmd = str
-MaxDuration = float
-Duration = float
-Text = str
-Orient = float[6]
-ActMount = float[nAct]
-Iter = int
-AxisHomed = int[nAct]
-Status = int[nAct]
-MaxIter = int
+    Key("Cmd", String(invalid = "?"), help = "The current command (verb) executing."),
+    Key("MaxDuration",
+        Float(invalid = "nan", units = "seconds"),
+        help = "Maximum amount of time expected for current process."
+    ),
+    Key("Duration",
+        Float(invalid = "nan", units = "seconds"),
+        help = "Time spent executing so far."
+    ),
+    Key("Text", String(), help = "Text for humans to read"),
+    Key("Orient",
+        Float(invalid = "nan", help = "Piston", units = "um"),
+        Float(invalid = "nan", help = "Tilt X", units = "arcseconds"),
+        Float(invalid = "nan", help = "Tilt Y", units = "arcseconds"),
+        Float(invalid = "nan", help = "Translation X", units = "um"),
+        Float(invalid = "nan", help = "Translation Y", units = "um"),
+        Float(invalid = "nan", help = "Rotation about Z", units = "arcseconds"),
+        help = "The measured mirror orientation."
+    ),
+    Key("ActMount", Float(invalid = "nan"), help = "The measured actuator mount positions")*(nAct,),
+    Key("Iter", Int(invalid = "nan"), help = "Current move iteration."),
+    Key("AxisHomed", Bool(0, 1, invalid = "?"), help = "Axes that are homed.")*(nAct,),
+    Key("Status", Bits("Stop Code:8", 
+                        "not used",
+                        "Home switch activated",
+                        "Reverse limit switch activated",
+                        "Forward limit switch activated",
+                        "undefined",
+                        "Motor on",
+                        "not used",
+                        "Axis in motion",
+                        "
+                        )
+    )*(nAct,]),
+    Key("MaxIter" = int
 Homing = int[nAct]
 UnparsedReply = str[, str]
 BadGalilReply = str, str
@@ -35,9 +62,9 @@ GParNAXES = int
 GParDOAUX = int
 GParMOFF = int
 GParNCORR = int
-GParWTIME = float
-GParENCTIME = float
-GParLSTIME = float
+GParWTIME = Float
+GParENCTIME = Float
+GParLSTIME = Float
 GPar_RNGxdiv2 = int[nAct]
 # these look equal but opposite, get rid of one?
 GParRNGxdiv2 = int[nAct]
@@ -49,6 +76,6 @@ GParMAXCORRx = int[nAct]
 GParST_FSx = int[nAct]
 GParMARGx = int[nAct]
 GParINDSEP = int[nAct]
-GParENCRESx = float[nAct]
+GParENCRESx = Float[nAct]
 
 )
