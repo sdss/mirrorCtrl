a5a3854247c09b90d5b086fc62fa4d7614c91064
Russell Owen <rowen@uw.edu>
2012-09-21 14:21:00 -0700

    Improved tests/testMirrorCtrl.py Moved fakeGalil into the main code library.

--- tests/testMirrorCtrl.py
+++ tests/testMirrorCtrl.py
@@ -1,11 +1,12 @@
 """http://twistedmatrix.com/documents/current/core/howto/trial.html
 """
-import fakeGalil
 from twisted.trial import unittest
 from twisted.internet.defer import Deferred, gatherResults, maybeDeferred
 from twisted.internet.protocol import ProcessProtocol, ServerFactory
+from twisted.internet import reactor
 import mirrorCtrl
 import mirrorCtrl.mirrors.mir25mSec
+from mirrorCtrl.fakeGalil import FakeGalilProtocol
 from twistedActor import ActorDevice
 from RO.Comm.TCPConnection import TCPConnection
 from opscore.actor import ActorDispatcher, CmdVar
@@ -21,16 +22,6 @@ Sec25mUserPort = 2532
 def showReply(msgStr, *args, **kwargs): # prints what the dispactcher sees to the screen
     print 'reply: ' + msgStr + "\n"
 
-def connCB(self, *args): # callback for TCP connection state change
-    print '%s Device state: %s' % (self._name, self.fullState)
-    if self.isConnected:
-        print '%s Device Connected on port %s' % (self._name, self.port)
-        d, self.connDeferred = self.connDeferred, None
-        d.callback('')
-    if self.fullState[0] == 'Disconnected':
-        d, self.disconnDeferred = self.disconnDeferred, None
-        d.callback('')
-
 
 
 class MirrorCtrlTestCase(unittest.TestCase):
@@ -39,43 +30,55 @@ class MirrorCtrlTestCase(unittest.TestCase):
     """
     timeout = 10 # ten seconds before timeout
 
+
     def setUp(self):
+        print "setUp()"
+        self.dispatcher = None
         # first start up the fake galil listening
-        self.setupGalil()
+        galilPort = self.startFakeGalil()
         # connect an actor to it
-        d = self.setupActor()
+        d = self.startMirrorCtrl(galilPort = galilPort)
         # after that connection is made, connect a dispatcher to the actor
-        d.addCallback(self.setupCommander)
+        d.addCallback(self.startCommander)
         return d
         
     def tearDown(self):
-        # don't exit until all connections are disconnected
-        # this may be unnecessary...
-        d1, d2 = self.actordisconn, self.cmddisconn
-        self.actordisconn, self.cmddisconn = None, None
-        return gatherResults([d1, d1])
+        """Tear down things; most of the work is done by addCleanup
+        """
+        print "tearDown()"
+        if self.dispatcher is not None:
+            self.dispatcher.disconnect()
 
-    def setupGalil(self):
-        self.factory = ServerFactory()
-        self.factory.protocol = fakeGalil.FakeGalilProtocol
-        from twisted.internet import reactor
-        self.galilport = reactor.listenTCP(0, self.factory, interface="localhost")
-        self.galilportnum = self.galilport.getHost().port
-        self.addCleanup(self.galilport.stopListening)
+    def startFakeGalil(self):
+        """Start the fake Galil on a randomly chosen port; return the port number
+        """
+        print "startFakeGalil()"
+        factory = ServerFactory()
+        factory.protocol = FakeGalilProtocol
+        portObj = reactor.listenTCP(0, factory, interface="localhost")
+        galilPort = portObj.getHost().port
+        self.addCleanup(portObj.stopListening)
+        print "Started fake Galil on port", galilPort
+        return galilPort
         
-    def setupActor(self):
-        d = Deferred()
-        self.actordisconn = Deferred()
+    def startMirrorCtrl(self, galilPort):
+        """Start mirror controller
+        """
+        print "startMirrorCtrl(galilPort=%r)" % (galilPort,)
         mirror = mirrorCtrl.mirrors.mir25mSec.Mirror
         self.mirDev = mirrorCtrl.GalilDevice25Sec(
             mirror = mirror,
             host = 'localhost',
-            port = self.galilportnum,            
-            )
-        self.mirDev.conn.connDeferred = d
-        self.mirDev.conn.disconnDeferred = self.actordisconn
-        self.mirDev.conn._name = 'Actor'
-        self.mirDev.conn.addStateCallback(connCB) # will callback upon connection
+            port = galilPort,            
+        )
+        
+        d = Deferred()
+        def connCallback(conn, d=d):
+            print "mirror controller conn state=", conn.state
+            if conn.isConnected:
+                d.callback("success")
+        
+        self.mirDev.conn.addStateCallback(connCallback)
         self.mirActor = mirrorCtrl.MirrorCtrl(
             device = self.mirDev,
             userPort = Sec25mUserPort        
@@ -83,26 +86,31 @@ class MirrorCtrlTestCase(unittest.TestCase):
         self.addCleanup(self.mirDev.conn.disconnect)
         self.addCleanup(self.mirActor.server.close)
         return d
-    
-    
-    def setupCommander(self, *args): # *args for the callback result that we don't care about
+
+    def startCommander(self, *args): # *args for the callback result that we don't care about
+        """Start a commander"""
+        print "startCommander(*args=%r)" % (args,)
         # this doesn't close cleanly
-        d = Deferred()
-        self.cmddisconn = Deferred()
         self.cmdConn = TCPConnection(
             host = 'localhost',
             port = Sec25mUserPort,
             readLines = True,
             name = "Commander",
         )
+
         self.dispatcher = ActorDispatcher(
             name = "mirror",
             connection = self.cmdConn,
             logFunc = showReply
         )
-        self.cmdConn.connDeferred = d
-        self.cmdConn.disconnDeferred = self.cmddisconn
-        self.cmdConn.addStateCallback(connCB)
+
+        d = Deferred()
+        def connCallback(conn, d=d):
+            print "commander conn state=", conn.state
+            if conn.isConnected:
+                d.callback("success")
+
+        self.cmdConn.addStateCallback(connCallback)
         self.cmdConn.connect()
         self.addCleanup(self.cmdConn.disconnect)
         return d    
